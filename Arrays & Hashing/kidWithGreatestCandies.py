a5538bd53b75be99f27aaa30f0ca039a3f4f43dd
Shashank P <85304058+shank-p@users.noreply.github.com>
2022-08-29 11:00:33 +0530

    initial commit

--- Arrays & Hashing/kidWithGreatestCandies.py
+++ Arrays & Hashing/kidWithGreatestCandies.py
@@ -6,3 +6,11 @@
 candies = list(map(int, input().split()))
 extraCandies = int(input())
 
+result = []
+curMax = max(candies)
+for i in candies:
+    if (i + extraCandies >= curMax):
+        result.append(True)
+    else:
+        result.append(False)
+print(result)
